9d00c8f06241f6f4feefb1dd6c08b6dc68f10267
kareembasher <kareem.basher@gmail.com>
2022-09-14 14:39:52 +0200

    Created a function that checks if the image is available.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
+import fs from 'fs-extra'
+import path from 'path'
 
 const port = 3000
 // create an instance server
 const app: Application = express()
 // HTTP request logger middleware
 app.use(morgan('short'))
+app.use(express.static('../assets'))
 
 // add routing for / path
-app.get('/', (req: Request, res: Response) => {
-  
+app.get('/api/images', async (req: Request, res: Response) => {
+  const filename = req.query.filename as string
+  const filePath = path.join(__dirname, '../assets/full', filename)
+  try {
+    const exists = await fs.pathExists(`${filePath}.jpg`)
+    if (!exists) throw 'Input file name not found'
+  } catch (err) {
+    res.send(err)
+  }
 })
 
 // start express server
